0072fa7d17bc7497b3f45057469df55f2006cd4e
FrankFang0830 <44310469+FrankFang0830@users.noreply.github.com>
2019-01-29 21:48:42 -0600

    Initial commit

---
+++ Python1.py
@@ -0,0 +1,28 @@
+print('I \'m Peihao Fang') # I'm Peihao Fang
+
+first=input("please input your first name")
+family=input("please input your family name")
+print(first[::-1],family[::-1])
+
+first=int(input("please input 1st number"))
+family=int(input("please input 2nd number"))
+print("The result by multiplication is",first*family)
+
+
+stc=input("please input a senctence")
+print(stc)
+isAlpha=0
+isDigtal=0
+isIllegal=0
+isSpace=0
+for aWord in stc:       #Once take out one character from stc give it to variable:aWord
+    if aWord.isdigit():
+        isDigtal +=1
+    elif aWord.isalnum():
+        isAlpha +=1
+    elif aWord.isspace():
+        isSpace +=1
+    else: isIllegal +=1
+print ("This sentence contains %d's digtals" %isDigtal)
+print ("This sentence contains %d's letters" %isAlpha)
+print ("This sentence contains %d's spaces" %isSpace)
---
+++ test.py
@@ -0,0 +1,41 @@
+# s3 = r'Hello, "Bart"'
+# s4 = r'''Hello,
+# Lisa!'''
+# print(s3,'\n',s4)
+# Airlines=['Air China','Eva Air','Air France','Uninted']
+# print(len(Airlines))
+# # print(Airlines[0],'is the top of the list')
+#
+# weight=float(input("please input your weight"))
+# height=float(input("please input your height"))
+# bMI=weight/(height*height)
+# print(bMI)
+# if bMI>18.5:
+#     print('too light')
+# elif 18.5<bMI<25:
+#     print('normal')
+# else:
+#     print("overweight")
+
+# num=[1,2,3,4,5]
+sum=0
+for i in range(1001):
+    sum=i+sum
+print(sum)
+#
+# n = 0
+# while n < 10:
+#     n = n + 1
+#     if n % 2 == 0: # 如果n是偶数，执行continue语句
+#         continue # continue语句会直接继续下一轮循环，后续的print()语句不会执行
+#     print(n)
+#
+# d={'fang':2,'pei':1,'hao':3}
+# print(d)
+# print(d['fang'])
+# d['fang']=11
+# print(d)
+# d['hoa']=9
+# print(d)
+# d.pop('hoa')
+# print(d)
